8a5f81b19b8253bf8ef25ae8c31836a626a92df2
TheBrothersPab <walley.jos@gmail.com>
2018-11-05 22:14:33 -0600

    Added validation for character info

--- app/Http/Controllers/characterInfo.php
+++ app/Http/Controllers/characterInfo.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\characterInfo as characterInfo_model;
 use Illuminate\Support\Facades\DB;
+use Validator;
+use Illuminate\Validation\Rule;
 
 class characterInfo extends Controller
 {
@@ -18,6 +20,30 @@ class characterInfo extends Controller
     }
 
     public function put(Request $request) {
+      Log::debug('PUT api/character-info', [
+        'request.body' => $request->getContent(),
+      ]);
+      $validator = Validator::make($request->all(), [
+        'name' => 'required|min:1|max:20',
+        'race' => ['required', Rule::in([
+          'dwarf', 'elf', 'halfling', 'human',
+          'dragonborn', 'gnome', 'half-elf', 'half-orc', 'tiefling'
+        ])],
+        'class' => ['required', Rule::in([
+          'barbarian', 'bard', 'cleric', 'druid',
+          'fighter', 'monk', 'paladin', 'ranger',
+          'rogue', 'sorcerer', 'warlock', 'wizard'
+        ])],
+        'sex' => 'required',
+      ]);
+
+      if ($validator->fails()) {
+        Log::debug('PUT api/character-info:Error', [
+          'errors' => $validator->errors(),
+        ]);
+        return json_encode($validator->errors());
+      }
+
       $body=$request->getContent();
       $body_decoded = json_decode($body);
 
